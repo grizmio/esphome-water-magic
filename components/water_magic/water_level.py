f202db1b671f7b5f65f6fbf32a36b77de9090fc2
grizmio <grizmio@gmail.com>
2025-07-19 01:13:22 -0400

    renombre

--- components/water_magic/water_level.py
+++ components/water_magic/water_level.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import display, sensor
+from esphome.components import display, output
 from esphome.const import (
     CONF_ID,
     CONF_LAMBDA,
@@ -45,7 +45,7 @@ CONFIG_SCHEMA = (
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID], "WaterMagic")
-    await sensor.register_component(var, config)
+    await output.register_component(var, config)
     cg.add_platformio_option("framework", "arduino")
     cg.add_build_flag("-DUSE_ARDUINO")
 
