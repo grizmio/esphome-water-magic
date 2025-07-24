b8b194650829aa4d8392cdd44681094a4e995b1d
GriZmio <grizmio@gmail.com>
2025-07-24 00:14:21 -0400

    Actualizar dht12.h

--- components/water_distance_dual/dht12.h
+++ components/water_distance_dual/dht12.h
@@ -2,12 +2,11 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
-#include "esphome/components/i2c/i2c.h"
 
 namespace esphome {
 namespace dht12 {
 
-class DHT12Component : public PollingComponent, public i2c::I2CDevice {
+class WaterDistanceComponent : public sensor::Sensor, public PollingComponent {
  public:
   void setup() override;
   void dump_config() override;
